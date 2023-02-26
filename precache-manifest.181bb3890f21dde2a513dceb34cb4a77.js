8720335dc7050010b2c758048e2261c3ebc0042e
Omar Abdelrahman <omarahmed8k@gmail.com>
2023-02-27 00:16:22 +0200

    Updates

---
+++ precache-manifest.181bb3890f21dde2a513dceb34cb4a77.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b2b4f7fa5ee8e987ac0",
+    "url": "/reusable-components/static/css/main.89a0706a.chunk.css"
+  },
+  {
+    "revision": "0b2b4f7fa5ee8e987ac0",
+    "url": "/reusable-components/static/js/main.a867ee07.chunk.js"
+  },
+  {
+    "revision": "6c31908c56d7ddc8df2d",
+    "url": "/reusable-components/static/js/runtime~main.f83f01da.js"
+  },
+  {
+    "revision": "7de766e09e29bd4f40f7",
+    "url": "/reusable-components/static/js/2.f8568428.chunk.js"
+  },
+  {
+    "revision": "eb11dd579357f10ff376407b2830c70f",
+    "url": "/reusable-components/static/media/logo-ar.eb11dd57.png"
+  },
+  {
+    "revision": "7074fd44b54d83bfa97bb41227f7725c",
+    "url": "/reusable-components/static/media/logo-en.7074fd44.png"
+  },
+  {
+    "revision": "3358032dd0994cf4a2116f0b16f80d70",
+    "url": "/reusable-components/static/media/Tajawal-Medium.3358032d.ttf"
+  },
+  {
+    "revision": "e3fe295c55a0cb720f766bccc5eecf63",
+    "url": "/reusable-components/static/media/Tajawal-Regular.e3fe295c.ttf"
+  },
+  {
+    "revision": "76f83be859d749342ba420e1bb010d6a",
+    "url": "/reusable-components/static/media/Tajawal-Bold.76f83be8.ttf"
+  },
+  {
+    "revision": "1c30954714d8c8b02ffb40b9bd84e089",
+    "url": "/reusable-components/static/media/close.1c309547.svg"
+  },
+  {
+    "revision": "96e7d47f048dd578f752cc96cc06295e",
+    "url": "/reusable-components/index.html"
+  }
+];
